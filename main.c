76830947525e5375bb601223eadba121703ddcb5
Eric Warmenhoven <eric@warmenhoven.org>
2006-09-14 20:17:00 +0000

    might help to actually include keepalive_user. also add a --no-server option for debugging the ui.

--- main.c
+++ main.c
@@ -5,13 +5,16 @@
 
 nbio_t gnb;
 struct session_info si;
+char keepalive_user[256];
 
-int main()
+int
+main(int argc, char **argv)
 {
 	time_t lastnop;
 	si.killme = 0;
 
 	memset(&si, 0, sizeof(si));
+	memset(keepalive_user, 0, sizeof(keepalive_user));
 
 	if (read_config())	/* this will also create a default one if it doesn't exist yet */
 		return 1;
@@ -22,8 +25,10 @@ int main()
 	if (init_window())	/* this will draw the initial window and add stdin to nbio */
 		return 1;
 
-	if (init_server())	/* this will add things to gnb. yay for globals */
-		return 1;
+	if (argc < 2 || strcmp(argv[1], "--no-server")) {
+		if (init_server())	/* this will add things to gnb. yay for globals */
+			return 1;
+	}
 
 	time(&lastnop);
 	while (!si.killme) {
